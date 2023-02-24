7a46c05b7670a9d0b44bf28fd21465479a717007
Luis <86859569+lsstomas@users.noreply.github.com>
2023-02-24 12:28:54 -0300

    Exercício 06

---
+++ ex06.dart
@@ -0,0 +1,8 @@
+// Exercício 06 - Write a dart program to generate multiplication tables of 5.
+
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    int tabuada = 5 * i;
+    print("5 x $i = $tabuada");
+  }
+}
