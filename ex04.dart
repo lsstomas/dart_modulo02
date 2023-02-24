ae48e45854efbeb156f978b6ab65423250d6c35b
Luis <86859569+lsstomas@users.noreply.github.com>
2023-02-24 10:51:03 -0300

    Exercício 04

---
+++ ex04.dart
@@ -0,0 +1,11 @@
+// Exercício 04 - Write a dart program to print your name 100 times.
+
+void main() {
+  String nome = 'Luis';
+  int i = 1;
+  
+  while (i <= 100){
+    print(nome);
+    i++;
+  }
+}
